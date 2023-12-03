cacff21e92445cbc18041778a65b44aa07facac6
Evan Thomas <ethomas@hiddenroad.com>
2023-12-03 14:40:58 -0800

    Cleanup day 3

--- src/d03/part1.rs
+++ src/d03/part1.rs
@@ -42,7 +42,7 @@ fn get_number_starting_at<'a>(grid: &Grid<'a>, (r, c): (usize, usize)) -> (usize
     let start_idx = r * (grid.width + 1) + c;
     let end_idx = r * (grid.width + 1) + end_c;
 
-    // copies to the heap. UNfortunate
+    // copies to the heap. Unfortunate
     let n: usize = grid.buffer[start_idx..end_idx]
         .iter()
         .collect::<String>()
@@ -53,34 +53,32 @@ fn get_number_starting_at<'a>(grid: &Grid<'a>, (r, c): (usize, usize)) -> (usize
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // map loc -> character
-    // Symbol map loc -> symbols
-    // symbols set
+    // 1. Parse the input string into a Grid : a &[char] with a .get(Loc) attribute
     let content = fs::read_to_string("src/d03/input")?;
     let buffer: &[char] = &content.chars().collect::<Vec<char>>();
-    let width = buffer
-        .iter()
-        .enumerate()
-        .find_map(|(i, ch)| if ch == &'\n' { Some(i) } else { None })
-        .unwrap_or(buffer.len());
-    let height = buffer.len() / (width + 1);
+    let grid: Grid = {
+        let width = buffer
+            .iter()
+            .enumerate()
+            .find_map(|(i, ch)| if ch == &'\n' { Some(i) } else { None })
+            .unwrap_or(buffer.len());
+        let height = buffer.len() / (width + 1);
 
-    let grid = Grid {
-        width,
-        height,
-        buffer,
+        Grid {
+            width,
+            height,
+            buffer,
+        }
     };
 
-    let mut c = 0;
-    let mut symbol_locations: HashSet<(usize, usize)> = HashSet::new();
-    let mut numbers: Vec<Number> = Vec::new();
-
-    // parse input. Popluate symbol_locations and numbers
-    for r in 0..height {
-        loop {
-            let current_char = grid.get((r, c));
-            match current_char {
-                _ if current_char.is_digit(10) => {
+    // 2. Collect all the "Numbers" : a list of structs which are just pointers into the Grid that
+    //    has a number
+    let numbers: Vec<Number> = {
+        let mut c = 0;
+        let mut numbers: Vec<Number> = Vec::new();
+        for r in 0..grid.height {
+            loop {
+                if grid.get((r, c)).is_digit(10) {
                     let (end_c, n) = get_number_starting_at(&grid, (r, c));
                     numbers.push(Number {
                         n,
@@ -89,19 +87,34 @@ fn main() -> Result<(), Box<dyn Error>> {
                     });
                     c = end_c - 1;
                 }
-                '.' => (),
-                _ => {
+                c += 1;
+                debug_assert!(c <= grid.width, "c = {} width = {}", c, grid.width);
+                if c >= grid.width {
+                    break;
+                }
+            }
+            c = 0;
+        }
+        numbers
+    };
+
+    // 3. Collect locations of all the symbols
+    let symbol_locations: HashSet<(usize, usize)> = {
+        let mut symbol_locations: HashSet<(usize, usize)> = HashSet::new();
+        for r in 0..grid.height {
+            for c in 0..grid.width {
+                let current_char = grid.get((r, c));
+                let is_symbol = !matches!(
+                    current_char,
+                    '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '.',
+                );
+                if is_symbol {
                     symbol_locations.insert((r, c));
                 }
             }
-            c += 1;
-            debug_assert!(c <= width, "c = {} width = {}", c, width);
-            if c >= width {
-                break;
-            }
         }
-        c = 0;
-    }
+        symbol_locations
+    };
 
     // given a location (r, c), return all adjacent locations and itself
     let adjacent_locations = |(r, c): (isize, isize)| -> Vec<(usize, usize)> {
@@ -121,8 +134,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             .any(|loc| symbol_locations.contains(&loc))
     };
 
+    // 4. Take the sum of all numbers that touch a symbol
     let filtered_numbers = numbers.iter().filter(|number| is_touching_symbol(number));
-
     let sum: usize = filtered_numbers.map(|number| number.n).sum();
     println!("{}", sum);
 
--- src/d03/part2.rs
+++ src/d03/part2.rs
@@ -1,5 +1,3 @@
-use itertools::iproduct;
-use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fs;
 
@@ -10,20 +8,6 @@ struct Grid<'a> {
 }
 
 type Loc = (usize, usize);
-
-impl<'a> Grid<'a> {
-    /// given a location (r, c), return all adjacent locations and itself
-    fn adjacent_locations(&self, (r, c): Loc) -> Vec<Loc> {
-        let (r, c) = (r as isize, c as isize);
-        iproduct!([-1, 0, 1], [-1, 0, 1])
-            .map(|(dr, dc): (isize, isize)| (r + dr, c + dc))
-            .filter(|&(r, c)| {
-                0 <= r && r < (self.height as isize) && 0 <= c && c < (self.width as isize)
-            })
-            .map(|(r, c)| (r as usize, c as usize))
-            .collect()
-    }
-}
 
 /// pointer to the number in the grid
 #[derive(Debug)]
@@ -39,8 +23,10 @@ impl Number {
         debug_assert!(start_row == end_row);
         (start_col..end_col).map(move |c| (start_row, c))
     }
+
     fn touches(&self, (r, c): Loc) -> bool {
-        let (r, c) = (r as isize, c as isize);
+        let r: isize = r.try_into().unwrap();
+        let c: isize = c.try_into().unwrap();
 
         // take the difference between the given loc an all locations for this number
         let mut differences = self
@@ -80,81 +66,74 @@ fn get_number_starting_at<'a>(grid: &Grid<'a>, (r, c): (usize, usize)) -> (usize
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // 1. Parse the input string into a Grid : a &[char] with a .get(Loc) attribute
     let content = fs::read_to_string("src/d03/input")?;
     let buffer: &[char] = &content.chars().collect::<Vec<char>>();
-    let width = buffer
-        .iter()
-        .enumerate()
-        .find_map(|(i, ch)| if ch == &'\n' { Some(i) } else { None })
-        .unwrap_or(buffer.len());
-    let height = buffer.len() / (width + 1);
+    let grid: Grid = {
+        let width = buffer
+            .iter()
+            .enumerate()
+            .find_map(|(i, ch)| if ch == &'\n' { Some(i) } else { None })
+            .unwrap_or(buffer.len());
+        let height = buffer.len() / (width + 1);
 
-    let grid = Grid {
-        width,
-        height,
-        buffer,
+        Grid {
+            width,
+            height,
+            buffer,
+        }
     };
 
-    // map from loc -> &Number
-    // gears: (loc, number1, number2)
-    // for g in gears: calculate ratio and add
-
-    let mut c = 0;
-    let mut numbers: Vec<Number> = Vec::new();
-    let mut number_map: HashMap<Loc, &Number> = HashMap::new();
-    let mut gears: Vec<(Loc, &Number, &Number)> = Vec::new();
-
-    // Popluate numbers
-    for r in 0..height {
-        loop {
-            if grid.get((r, c)).is_digit(10) {
-                let (end_c, n) = get_number_starting_at(&grid, (r, c));
-                numbers.push(Number {
-                    n,
-                    start_loc: (r, c),
-                    end_loc: (r, end_c),
-                });
-                c = end_c - 1;
+    // 2. Collect all the "Numbers" : a list of structs which are just pointers into the Grid that
+    //    has a number
+    let numbers: Vec<Number> = {
+        let mut c = 0;
+        let mut numbers: Vec<Number> = Vec::new();
+        for r in 0..grid.height {
+            loop {
+                if grid.get((r, c)).is_digit(10) {
+                    let (end_c, n) = get_number_starting_at(&grid, (r, c));
+                    numbers.push(Number {
+                        n,
+                        start_loc: (r, c),
+                        end_loc: (r, end_c),
+                    });
+                    c = end_c - 1;
+                }
+                c += 1;
+                debug_assert!(c <= grid.width, "c = {} width = {}", c, grid.width);
+                if c >= grid.width {
+                    break;
+                }
             }
-            c += 1;
-            debug_assert!(c <= width, "c = {} width = {}", c, width);
-            if c >= width {
-                break;
-            }
+            c = 0;
         }
-        c = 0;
-    }
+        numbers
+    };
 
+    // 3. Collect a list of all the "Gears" : any '*' in the grid that touches exactly 2 numbers
     // Populate gears
-    for r in 0..height {
-        for c in 0..width {
-            if grid.get((r, c)) == '*' {
-                let touching_numbers: Vec<&Number> = numbers
-                    .iter()
-                    .filter(|number| number.touches((r, c)))
-                    .collect();
-                if touching_numbers.len() == 2 {
-                    gears.push(((r, c), touching_numbers[0], touching_numbers[1]));
+    let gears: Vec<(Loc, &Number, &Number)> = {
+        let mut gears = Vec::new();
+        for r in 0..grid.height {
+            for c in 0..grid.width {
+                if grid.get((r, c)) == '*' {
+                    let touching_numbers: Vec<&Number> = numbers
+                        .iter()
+                        .filter(|number| number.touches((r, c)))
+                        .collect();
+                    if touching_numbers.len() == 2 {
+                        gears.push(((r, c), touching_numbers[0], touching_numbers[1]));
+                    }
                 }
             }
         }
-    }
+        gears
+    };
 
-    // For each gear, multiply its numbers and add them all up
+    // 4. For each gear, calculate the product of its two adjacent numbers and sum it all up
     let n: usize = gears.iter().map(|gear| gear.1.n * gear.2.n).sum();
     println!("{}", n);
-
-    // given a number, look to see if it's touching any symbols
-    // let is_touching_symbol = |n: &Number| -> bool {
-    //     n.locations()
-    //         .flat_map(|(r, c)| adjacent_locations((r as isize, c as isize)))
-    //         .any(|loc| symbol_locations.contains(&loc))
-    // };
-
-    // let filtered_numbers = numbers.iter().filter(|number| is_touching_symbol(number));
-
-    // let sum: usize = filtered_numbers.map(|number| number.n).sum();
-    // println!("{}", sum);
 
     Ok(())
 }
