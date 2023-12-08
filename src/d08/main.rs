49deb47ee906fee0941c34da6bebf82d4a892e14
Evan Thomas <ethomas@hiddenroad.com>
2023-12-08 09:56:21 -0800

    Day 08 part 2 slow

--- src/d08/main.rs
+++ src/d08/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 
 use nom::{
     bytes::complete::tag,
-    character::complete::{alpha1, line_ending, space0},
+    character::complete::{alpha1, alphanumeric1, line_ending, space0},
     combinator::{all_consuming, map},
     multi::{count, many0, separated_list1},
     sequence::tuple,
@@ -21,15 +21,15 @@ fn parse(input: &str) -> IResult<&str, (&str, Map)> {
 
     let parse_line = map(
         tuple((
-            alpha1,
+            alphanumeric1,
             space0,
             tag("="),
             space0,
             tag("("),
-            alpha1,
+            alphanumeric1,
             tag(","),
             space0,
-            alpha1,
+            alphanumeric1,
             tag(")"),
         )),
         |(src, _, _, _, _, left, _, _, right, _)| (src, (left, right)),
@@ -47,23 +47,44 @@ fn main() -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string("src/d08/input")?;
     let (_, (instructions, map)) = all_consuming(parse)(&content).unwrap();
 
-    let mut i = 0;
-    let mut current = "AAA";
-    'outer: loop {
-        for x in instructions.chars() {
-            if current == "ZZZ" {
-                break 'outer;
-            }
-            match x {
-                'L' => current = map.get(current).unwrap().0,
-
-                'R' => current = map.get(current).unwrap().1,
-                _ => panic!("unexpected character {}", x),
-            }
-            i += 1;
+    let mut iterators = Vec::new();
+    for start in map.keys() {
+        if start.chars().nth(2).unwrap() == 'A' {
+            let mut current = *start;
+            let mut instruction_iterator = instructions.chars().cycle();
+            let map = &map;
+            let iter = std::iter::from_fn(move || {
+                let instruction = instruction_iterator.next().unwrap();
+                match instruction {
+                    'L' => current = &map.get(current).unwrap().0,
+                    'R' => current = &map.get(current).unwrap().1,
+                    _ => panic!("unexpected character {}", instruction),
+                };
+                Some(current)
+            });
+            // TODO: why don't i have to clone current
+            iterators.push(std::iter::once(current).chain(iter));
         }
     }
-    println!("{}", i);
+
+    // think it might be slow now becaues i'm doing a bunch of memcopys instaed of just all
+    let mut i: usize = 0;
+    let mut state = Vec::new();
+    loop {
+        state.truncate(0);
+        state.extend(iterators.iter_mut().map(|iter| iter.next().unwrap()));
+
+        i += 1;
+        if i % 50_000_000 == 0 {
+            println!("{}", i);
+        }
+
+        if state.iter().all(|x| x.chars().nth(2).unwrap() == 'Z') {
+            break;
+        }
+    }
+
+    println!("Answer :: {}", i);
 
     Ok(())
 }
