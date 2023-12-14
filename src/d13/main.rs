7eae51c0c44b9386dbcd311220f23399dc8097de
Evan Thomas <ethomas@hiddenroad.com>
2023-12-13 22:04:49 -0800

    Day 13 part 2

--- src/d13/main.rs
+++ src/d13/main.rs
@@ -1,3 +1,4 @@
+use itertools::iproduct;
 use std::error::Error;
 use std::fs;
 
@@ -19,16 +20,27 @@ fn find_reflection_lines<T: Eq>(v: &Vec<T>) -> impl Iterator<Item = usize> + '_ 
     (1..v.len()).filter(|&col| check_is_reflection_line(v, col))
 }
 
-fn find_first_vertical_reflection<T: Eq>(vec: &Grid<T>) -> Option<usize> {
+fn find_vertical_reflection_lines<T: Eq>(vec: &Grid<T>) -> Vec<usize> {
     let width = vec[0].len();
-    (1..width).find(|&col| vec.iter().all(|v| check_is_reflection_line(v, col)))
+    (1..width)
+        .filter(|&col| vec.iter().all(|v| check_is_reflection_line(v, col)))
+        .collect()
+}
+
+fn find_first_vertical_reflection<T: Eq>(vec: &Grid<T>) -> Option<usize> {
+    find_vertical_reflection_lines(vec).first().copied()
+}
+
+fn find_horizontal_reflection_lines<T: Eq + Clone>(vec: &Grid<T>) -> Vec<usize> {
+    find_vertical_reflection_lines(&transpose(&vec))
 }
 
 fn find_first_horizontal_reflection<T: Eq + Clone>(vec: &Grid<T>) -> Option<usize> {
-    find_first_vertical_reflection(&transpose(&vec))
+    find_horizontal_reflection_lines(vec).first().copied()
 }
 
 fn transpose<T: Clone>(v: &Grid<T>) -> Grid<T> {
+    // TODO: instead of allocating a new grid return a view on top of the current grid
     let height = v.len();
     let width = v[0].len();
     let mut ret: Grid<T> = (0..width).map(|_| Vec::with_capacity(height)).collect();
@@ -55,18 +67,52 @@ fn parse(content: &str) -> impl Iterator<Item = Grid<char>> + '_ {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string("src/d13/input")?;
-    let grids = parse(&content);
-    let mut sum: usize = 0;
-    for grid in grids {
+    let grids: Vec<_> = parse(&content).collect();
+
+    // part 1
+    let mut sum1: usize = 0;
+    let mut original_reflection_lines = Vec::new();
+    for grid in grids.iter() {
         if let Some(reflection_line) = find_first_vertical_reflection(&grid) {
-            sum += reflection_line;
+            sum1 += reflection_line;
+            original_reflection_lines.push(('v', reflection_line));
         } else if let Some(reflection_line) = find_first_horizontal_reflection(&grid) {
-            sum += 100 * reflection_line;
+            sum1 += 100 * reflection_line;
+            original_reflection_lines.push(('h', reflection_line));
         } else {
             panic!("oh no");
         }
     }
-    println!("{:?}", sum);
+    println!("part 1 {:?}", sum1);
+
+    // part 2
+    let mut sum2: usize = 0;
+    for (grid_idx, grid) in grids.iter().enumerate() {
+        let (width, height) = (grid[0].len(), grid.len());
+        let (ch, l) = iproduct!(0..height, 0..width)
+            .flat_map(|(r, c)| {
+                let mut new_grid = grid.clone();
+                new_grid[r][c] = if grid[r][c] == '#' { '.' } else { '#' };
+
+                let new_veritical_reflection_lines = find_vertical_reflection_lines(&new_grid)
+                    .into_iter()
+                    .map(|line| ('v', line));
+                let new_horizontal_reflection_lines = find_horizontal_reflection_lines(&new_grid)
+                    .into_iter()
+                    .map(|line| ('h', line));
+
+                new_veritical_reflection_lines.chain(new_horizontal_reflection_lines)
+            })
+            .find(|&x| x != original_reflection_lines[grid_idx])
+            .unwrap();
+
+        if ch == 'v' {
+            sum2 += l;
+        } else {
+            sum2 += 100 * l;
+        }
+    }
+    println!("part 2 {:?}", sum2);
     Ok(())
 }
 
