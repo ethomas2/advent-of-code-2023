86714f3e511f26ad765345d229784aae82798617
Evan Thomas <ethomas@hiddenroad.com>
2024-01-02 16:28:45 -0600

    Day 19 part 2

---
+++ src/d19/ndrange.rs
@@ -0,0 +1,143 @@
+// TODO: generalize start: T, end: T where they all are Eq, and Copy
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct Range {
+    pub start: usize,
+    pub end: usize,
+}
+
+/// A 1D Range [start, end)
+impl Range {
+    pub fn new(start: usize, end: usize) -> Self {
+        debug_assert!(end >= start);
+        Self { start, end }
+    }
+}
+
+// TODO: generalize this for ranges up to size 16. Ask chatgpt how. Use a macro?
+/// An NDRange ( [start1, end1), [start2, end2) ...). Forms an n-dimensional box. n is currently
+/// hardcoded to 4, but should be generialized with const generics
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct NDRange(pub [Range; 4]);
+
+impl Range {
+    pub fn split(&self, val: usize) -> (Self, Self) {
+        let &Range { start, end } = self;
+        let split_point = val.clamp(start, end);
+        let (left, right) = (Range::new(start, split_point), Range::new(split_point, end));
+        (left, right)
+    }
+}
+
+impl NDRange {
+    pub fn new(data: [Range; 4]) -> Self {
+        NDRange(data)
+    }
+    pub fn is_empty(&self) -> bool {
+        self.0.iter().all(|Range { start, end }| start == end)
+    }
+
+    pub fn split(&self, dimension: usize, val: usize) -> (Self, Self) {
+        let mut left = self.clone();
+        let mut right = self.clone();
+        let (left_range, right_range) = self.0[dimension].split(val);
+        left.0[dimension] = left_range;
+        right.0[dimension] = right_range;
+        (left, right)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_split_range() {
+        let range = Range::new(1, 10);
+        assert_eq!(range.split(0), (Range::new(1, 1), Range::new(1, 10)));
+        assert_eq!(range.split(1), (Range::new(1, 1), Range::new(1, 10)));
+        assert_eq!(range.split(5), (Range::new(1, 5), Range::new(5, 10)));
+        assert_eq!(range.split(10), (Range::new(1, 10), Range::new(10, 10)));
+        assert_eq!(range.split(11), (Range::new(1, 10), Range::new(10, 10)));
+    }
+
+    #[test]
+    fn test_split_ndrange() {
+        let ndrange = NDRange::new([
+            Range::new(1, 10),
+            Range::new(1, 10),
+            Range::new(1, 10),
+            Range::new(1, 10),
+        ]);
+
+        assert_eq!(
+            ndrange.split(0, 0),
+            (
+                NDRange::new([
+                    Range::new(1, 1),
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                ]),
+                NDRange::new([
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                ])
+            )
+        );
+
+        assert_eq!(
+            ndrange.split(0, 5),
+            (
+                NDRange::new([
+                    Range::new(1, 5),
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                ]),
+                NDRange::new([
+                    Range::new(5, 10),
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                ])
+            )
+        );
+
+        assert_eq!(
+            ndrange.split(0, 10),
+            (
+                NDRange::new([
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                ]),
+                NDRange::new([
+                    Range::new(10, 10),
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                ])
+            )
+        );
+
+        assert_eq!(
+            ndrange.split(1, 5),
+            (
+                NDRange::new([
+                    Range::new(1, 10),
+                    Range::new(1, 5),
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                ]),
+                NDRange::new([
+                    Range::new(1, 10),
+                    Range::new(5, 10),
+                    Range::new(1, 10),
+                    Range::new(1, 10),
+                ])
+            )
+        );
+    }
+}
